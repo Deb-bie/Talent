c8cd7b8067e85611e2a43b989b0e614d68c8d903
Debbie <asamoahjaeldeborah@gmail.com>
2022-03-01 21:50:20 +0000

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Home from './pages/home/home';
 
 function App() {
   return (
-    <div className="App">
-      heyy talent
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={ <Home /> } />
+      </Routes>
+    </Router>
   );
 }
 
