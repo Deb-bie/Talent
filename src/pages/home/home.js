b39c6a91f8df2cd4295a63ffdf9dcc02349eab0f
Debbie <asamoahjaeldeborah@gmail.com>
2022-03-01 21:55:45 +0000

    Updated the home page

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,10 +1,19 @@
 import React from 'react'
+import Hero from '../../components/hero/hero';
 import Navbar from '../../components/navbar/navbar';
+import Packages from '../../components/packages/packages';
+
+
+
 
 const Home = () => {
   return (
     <div>
        <Navbar />
+
+       <Hero />
+
+       <Packages />
     </div>
   )
 }
